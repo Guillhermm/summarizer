66813097487d059a3760da5daf8dc8dd03f93833
Guillhermm <guillhermm_zenni@hotmail.com>
2024-06-09 16:37:30 -0300

    Add Hover BG Color To Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,10 @@ interface ButtonProps {
 
 export const Button = ({ children, className, onClick }: ButtonProps) => (
   <button
-    className={classnames('p-2 bg-blue-500 text-white rounded cursor-pointer', className)}
+    className={classnames(
+      'p-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600',
+      className
+    )}
     onClick={onClick}
   >
     {children}
