250960167bed5977bb57e57be844acaeb26d5eb2
Guillhermm <guillhermm_zenni@hotmail.com>
2024-06-10 23:33:03 -0300

    Decouple Close Icon

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IconClose } from './Icons/IconClose';
 
 interface ModalProps {
   onClose: () => void;
@@ -14,20 +15,7 @@ export const Modal = ({ onClose, summarizedText }: ModalProps) => {
           className="tw-summarizer-absolute tw-summarizer-top-0 tw-summarizer-right-0 tw-summarizer-m-3 tw-summarizer-p-1 tw-summarizer-rounded-full"
           onClick={onClose}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <IconClose className="tw-summarizer-h-6 tw-summarizer-w-6" />
         </button>
         <div className="modal-body">
           <h1 className="tw-summarizer-text-xl tw-summarizer-font-bold tw-summarizer-mb-4">
